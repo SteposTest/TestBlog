fa9ee556f5e555b9ba994cc61fa21063260fecdc
stepos <Stepan.Poshibaylov@transas.com>
2018-02-13 19:36:29 +0300

    add login

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import PostViews
+from .views import LoginViews, PostViews
 
 urlpatterns = [
+    url(r'^login/$', LoginViews.as_view(), name='login_views'),
     url(r'^', PostViews.as_view(), name='posts_view')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.views.generic import View, list
+
 from .models import Post
-from django.views.generic.list import ListView
 
 
-class PostViews(ListView):
+class PostViews(list.ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'posts.html'
 
     def get_queryset(self):
         return Post.objects.all()
+
+
+class LoginViews(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request, onsuccess='/', onfail='/login/'):
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect(onsuccess)
+        else:
+            return redirect(onfail)
