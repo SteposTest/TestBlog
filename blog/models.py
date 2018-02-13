221af088e6a3bcafb514986cda0268fbf8cf5536
stepos <Stepan.Poshibaylov@transas.com>
2018-02-13 14:56:42 +0300

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
+from jsonfield import JSONField
 
-# Create your models here.
+MAX_LENGTH = 255
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    posts = models.ForeignKey('Post', on_delete=models.CASCADE, blank=True, null=True)
+    viewed = JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.pk}: {self.user.username}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=MAX_LENGTH, unique=True)
+    text = models.TextField(max_length=MAX_LENGTH)
+    created = models.TimeField(auto_created=True)
+
+    def __str__(self):
+        return f'{self.pk}: {self.title}'
--- test_blog/wsgi.py
+++ test_blog/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for test_blog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
+from importlib import import_module
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_blog.settings")
 
 application = get_wsgi_application()
+
+import_module('blog.utils.models_signal_utils')
