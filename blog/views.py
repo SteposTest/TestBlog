5c03722f8ceabe80972c3d7fce820fbcbddba0e2
stepos <Stepan.Poshibaylov@transas.com>
2018-02-13 22:23:00 +0300

    add create post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-from django.views.generic import View, list
+from django.views.generic import View, list, CreateView
 
 from .models import Post
 
@@ -8,7 +8,10 @@ from .models import Post
 class PostViews(list.ListView):
     model = Post
     context_object_name = 'posts'
-    template_name = 'posts.html'
+
+    def get_template_names(self):
+        auth = self.request.user.is_authenticated()
+        return 'user_posts.html' if auth else 'all_posts.html'
 
     def get_queryset(self):
         return Post.objects.all()
@@ -25,3 +28,23 @@ class LoginViews(View):
             return redirect(onsuccess)
         else:
             return redirect(onfail)
+
+
+class CreatePostViews(CreateView):
+    model = Post
+    template_name = 'create_post.html'
+    fields = ['title', 'text']
+
+    def post(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated():
+            return redirect('/login/')
+        try:
+            result = request.POST
+            Post.objects.create(
+                title=result['title'],
+                text=result['text'],
+                user_profile=request.user.profile
+            )
+            return redirect('/')
+        except:
+            return redirect('/create_post/')
